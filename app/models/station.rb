04f36825ed466ba4fa2a2c52548b40b9169c34b6
William Fischer <william.fischer42@gmail.com>
2018-12-12 10:25:43 -0700

    Added api call method

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,9 +1,7 @@
 class Station
 
-  def self.all_near(zipcode)
-    # Connect to Faraday
-    # Make API Call
-    @api_response ||= get_all_stations_near(zipcode)
+  def self.all_near(zipcode, limit: 10)
+    @api_response ||= get_nearest_stations(location: zipcode, limit: limit)
   end
 
   def name
@@ -11,7 +9,13 @@ class Station
   end
 
   private
-    def self.get_all_stations()
+    def self.get_nearest_stations(params)
+      params[:api_key] = ENV['NREL_API_KEY']
+      url = "https://developer.nrel.gov/api/alt-fuel-stations/v1/"
+      conn = Faraday.new(url: url)
+      response = conn.get 'nearest.json', params
 
+      parsed = JSON.parse(response.body, symbolize_names: true)
+      station_data = parsed[:fuel_stations]
     end
 end
